8a8c14c6fa06dde9d55260b71e5c661e8b1f6926
EC2 Default User <ec2-user@ip-10-0-1-56.ap-south-1.compute.internal>
2026-08-04 06:44:58 +0000

    added all scripts

---
+++ check_user_exists.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+<<info
+This shell script checks if user exists
+info
+
+read -p "Enter the username you wish to check" username
+
+count=$(cat /etc/passwd | grep $username | wc | awk '{print $1}')
+
+echo "$count"
+
+if [ $count == 0 ];
+then
+	echo " User does not exist"
+else
+	echo "user $username exists"
+fi
+
+
+
